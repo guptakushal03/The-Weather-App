d44c163aadd8a388155be434ec1ca4258d089846
Kushal Gupta <98078018+guptakushal03@users.noreply.github.com>
2024-03-24 13:43:22 +0530

    date format change

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
 
     if (index === 0) {
         return `<div class="details">
-                    <h2>${cityName} (${formatDate(weatherItem.dt_txt.split(" ")[0])})</h2>
+                    <h2>${cityName} [${formatDate(weatherItem.dt_txt.split(" ")[0])}]</h2>
                     <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
@@ -25,7 +25,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
                 </div>`;
     } else {
         return `<li class="card">
-                    <h3>(${formatDate(weatherItem.dt_txt.split(" ")[0])})</h3>
+                    <h3>[${formatDate(weatherItem.dt_txt.split(" ")[0])}]</h3>
                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}.png" alt="weather-icon">
                     <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
